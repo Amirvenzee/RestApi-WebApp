b585e6457003f907a01c5c46229ac578c54d23b3
Amir hosein <121675889+Amirvenzee@users.noreply.github.com>
2025-08-09 17:19:15 +0330

    Add Auth Controller

---
+++ VILA.Web/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VILA.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
