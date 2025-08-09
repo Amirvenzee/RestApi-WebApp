90ad58c6058a5745e7c28435da38c9e4c885174f
Amir hosein <121675889+Amirvenzee@users.noreply.github.com>
2025-08-09 17:31:16 +0330

    comment

--- VILA.Web/Controllers/AuthController.cs
+++ VILA.Web/Controllers/AuthController.cs
@@ -4,6 +4,7 @@ namespace VILA.Web.Controllers
 {
     public class AuthController : Controller
     {
+        //Comment
         public IActionResult Register()
         {
             return View();
