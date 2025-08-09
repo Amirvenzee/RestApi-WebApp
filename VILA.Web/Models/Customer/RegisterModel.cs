e22835bec1bd154d560aa016e068a37bf34ff49d
Amir hosein <121675889+Amirvenzee@users.noreply.github.com>
2025-08-09 17:23:18 +0330

    Add UI

---
+++ VILA.Web/Models/Customer/RegisterModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VILA.Web.Models.Customer
+{
+    public class RegisterModel
+    {
+        [Display(Name = "موبایل")]
+        [Required(ErrorMessage = "موبایل اجباری است")]
+        [MaxLength(11, ErrorMessage = "موبایل باید 11 رقم باشد .")]
+        [MinLength(11, ErrorMessage = "موبایل باید 11 رقم باشد .")]
+        public string Mobile { get; set; }
+
+
+        [Display(Name = "رمز عبور ")]
+        [Required(ErrorMessage = "رمز عبور اجباری است")]
+        [MaxLength(90)]
+        public string Pass { get; set; }
+    }
+}
--- VILA.Web/wwwroot/lib/bootstrap/dist/js/bootstrap.bundle.js
+++ VILA.Web/wwwroot/lib/bootstrap/dist/js/bootstrap.bundle.js
@@ -1902,7 +1902,7 @@
 
   function getLayoutRect(element) {
     var clientRect = getBoundingClientRect(element); // Use the clientRect sizes if it's not been transformed.
-    // Fixes https://github.com/popperjs/popper-core/issues/1223
+    // Fixes https://github.com/popper/Content/js/popper-core/issues/1223
 
     var width = element.offsetWidth;
     var height = element.offsetHeight;
@@ -1978,7 +1978,7 @@
   }
 
   function getTrueOffsetParent(element) {
-    if (!isHTMLElement(element) || // https://github.com/popperjs/popper-core/issues/837
+    if (!isHTMLElement(element) || // https://github.com/popper/Content/js/popper-core/issues/837
     getComputedStyle$1(element).position === 'fixed') {
       return null;
     }
@@ -3186,7 +3186,7 @@
     };
 
     if (isOffsetParentAnElement || !isOffsetParentAnElement && !isFixed) {
-      if (getNodeName(offsetParent) !== 'body' || // https://github.com/popperjs/popper-core/issues/1078
+      if (getNodeName(offsetParent) !== 'body' || // https://github.com/popper/Content/js/popper-core/issues/1078
       isScrollParent(documentElement)) {
         scroll = getNodeScroll(offsetParent);
       }
