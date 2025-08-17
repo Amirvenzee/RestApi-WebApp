bfb81d2e72d0b6e7eecdc1c695e1cd098dd1edb8
Amir hosein <121675889+Amirvenzee@users.noreply.github.com>
2025-08-17 16:17:30 +0330

    Add VilaPaging

---
+++ VILA.Web/Models/Vila/VilaPaging.cs
@@ -0,0 +1,16 @@
+﻿namespace VILA.Web.Models.Vila
+{
+    public class VilaPaging
+    {
+        public int PageCount { get; set; }
+        //صفحه چندم
+        public int PageId { get; set; }
+        public int TotalData { get; set; }
+        public int Take { get; set; }
+        public int StartPage { get; set; }
+        public int EndPage { get; set; }
+
+        public List<VilaSearchModel> Vilas { get; set; }
+        public string Filter { get; set; }
+    }
+}
