cb584971eb97e6a898876e19b439b4b6ce8d177c
Amir hosein <121675889+Amirvenzee@users.noreply.github.com>
2025-08-17 16:48:11 +0330

    Add Vila Adresses

--- VILA.Web/Utility/ApiUrls.cs
+++ VILA.Web/Utility/ApiUrls.cs
@@ -4,6 +4,10 @@
     {
         public string BaseAddress { get; set; }
         public string CustomerAddress { get; set; }
+        public string VilaV1Address { get; set; }
+        public string VilaV2Address { get; set; }
+        public string VilaDetailAddress { get; set; }
+      
 
     }
 }
