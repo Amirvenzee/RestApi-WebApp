06b668855e927beee21663cad9c197abf86ec897
Amir hosein <121675889+Amirvenzee@users.noreply.github.com>
2025-08-20 15:56:02 +0330

    Complete HomeController

--- VILA.Web/Controllers/HomeController.cs
+++ VILA.Web/Controllers/HomeController.cs
@@ -1,21 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using VILA.Web.Models;
+using VILA.Web.Services.Customer;
+using VILA.Web.Services.Vila;
 
 namespace VILA.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IVilaRepository _vila;
+        private readonly IAuthService _auth;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IVilaRepository vila, IAuthService auth)
         {
-            _logger = logger;
+            _vila = vila;
+            _auth = auth;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int pageId = 1 ,string filter = "", int take = 6)
         {
-            return View();
+            // string token = HttpContext.Session.GetString("JWTSecret");
+
+            var token = _auth.GetJwtToken();
+            var model = await _vila.Search(pageId,filter,take,token);
+            return View(model);
         }
 
         public IActionResult Privacy()
