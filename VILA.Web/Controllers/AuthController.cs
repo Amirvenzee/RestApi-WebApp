3174e73dc6c786839a50445aef5976b355523348
Amir hosein <121675889+Amirvenzee@users.noreply.github.com>
2025-08-20 16:01:11 +0330

    Complete Login Method

--- VILA.Web/Controllers/AuthController.cs
+++ VILA.Web/Controllers/AuthController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
 using System.Runtime.InteropServices;
+using System.Security.Claims;
+using System.Security.Principal;
 using VILA.Web.Models.Customer;
 using VILA.Web.Services.Customer;
 
@@ -27,7 +31,7 @@ namespace VILA.Web.Controllers
                 return View(model);
                  
               var res =  await _customer.Register(model);
-            if(res.result)
+            if(res.Result)
             {
                 TempData["success"] = true;
                return View();
@@ -35,5 +39,42 @@ namespace VILA.Web.Controllers
             ModelState.AddModelError("", res.Message);
             return View(model);
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var res = await _customer.Login(model);
+            if (!res.Result.Result)
+            {
+
+                ModelState.AddModelError("", res.Result.Message);
+                return View(model);
+              
+            }
+            var customer = res.Customer;
+
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            identity.AddClaim(new Claim(ClaimTypes.Name, customer.Mobile));
+            identity.AddClaim(new Claim(ClaimTypes.Role, customer.Role));
+            identity.AddClaim(new Claim("JWTSecret", customer.JwtSecret));
+
+
+
+            var principal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+            return Redirect("/");
+        }
+
     }
 }
