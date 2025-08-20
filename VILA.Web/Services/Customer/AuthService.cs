e556e7d19ad36a1860db61f26a24de31c0f58737
Amir hosein <121675889+Amirvenzee@users.noreply.github.com>
2025-08-20 15:41:18 +0330

    Complete AuthService

--- VILA.Web/Services/Customer/AuthService.cs
+++ VILA.Web/Services/Customer/AuthService.cs
@@ -1,7 +1,28 @@
-﻿namespace VILA.Web.Services.Customer
-{
-    public class AuthService:IAuthService
-    {
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
 
+namespace VILA.Web.Services.Customer
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IHttpContextAccessor _context;
+
+        public AuthService(IHttpContextAccessor contextAccessor)
+        {
+            _context = contextAccessor;
+        }
+
+        public string GetJwtToken()
+        {
+           var claims = _context.HttpContext.User.Claims.ToList();
+            if (claims.Count() < 1) return "";
+            return _context.HttpContext.User.Claims.FirstOrDefault(x => x.Type == "JWTSecret").Value;
+        }
+
+        public void Logout()
+        {
+            _context.HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
     }
 }
--- VILA.Web/Services/Customer/IAuthService.cs
+++ VILA.Web/Services/Customer/IAuthService.cs
@@ -2,6 +2,8 @@
 {
     public interface IAuthService
     {
-        
+
+        string GetJwtToken();
+        void Logout();
     }
 }
