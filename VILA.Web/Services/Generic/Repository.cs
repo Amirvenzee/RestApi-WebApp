96459fe9917328031c8f00d55be4684bd4736081
Amir hosein <121675889+Amirvenzee@users.noreply.github.com>
2025-08-24 18:27:06 +0330

    Complete Generic Repository

--- VILA.Web/Services/Generic/Repository.cs
+++ VILA.Web/Services/Generic/Repository.cs
@@ -1,4 +1,7 @@
-﻿namespace VILA.Web.Services.Generic
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace VILA.Web.Services.Generic
 {
     public class Repository<T> : IRepository<T> where T : class
     {
@@ -8,29 +11,96 @@
             _client = client;
         }
 
-        public Task<bool> Create(string url, string token, T model)
+        public async Task<bool> Create(string url, string token, T model)
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+
+            request.Content = new StringContent
+                (JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+
+            var myClient = _client.CreateClient();
+
+            myClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+            HttpResponseMessage responseMessage = await myClient.SendAsync(request);
+
+            if (responseMessage.StatusCode == System.Net.HttpStatusCode.Created)
+                return true;
+
+            return false;
         }
 
-        public Task<bool> Delete(string url, string token)
+        public async Task<bool> Delete(string url, string token)
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Delete, url);
+
+            var myClient = _client.CreateClient();
+
+            myClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+            HttpResponseMessage responseMessage = await myClient.SendAsync(request);
+
+            if (responseMessage.StatusCode == System.Net.HttpStatusCode.NoContent)
+                return true;
+
+            return false;
         }
 
-        public Task<List<T>> GetAll(string url, string token)
+        public async Task<List<T>> GetAll(string url, string token)
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+            var myClient = _client.CreateClient();
+
+            myClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+            HttpResponseMessage responseMessage = await myClient.SendAsync(request);
+
+            if (responseMessage.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var json = await responseMessage.Content.ReadAsStringAsync();
+                var res = JsonConvert.DeserializeObject<List<T>>(json);
+                return res;
+            }
+            return null;
         }
 
-        public Task<T> GetById(string url, string token)
+        public async Task<T> GetById(string url, string token)
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+            var myClient = _client.CreateClient();
+
+            myClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+            HttpResponseMessage responseMessage = await myClient.SendAsync(request);
+
+            if (responseMessage.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var json = await responseMessage.Content.ReadAsStringAsync();
+                var res = JsonConvert.DeserializeObject<T>(json);
+                return res;
+            }
+            return null;
         }
 
-        public Task<bool> Update(string url, string token, T model)
+        public async Task<bool> Update(string url, string token, T model)
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Patch, url);
+
+            request.Content = new StringContent
+                (JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+
+            var myClient = _client.CreateClient();
+
+            myClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+            HttpResponseMessage responseMessage = await myClient.SendAsync(request);
+
+            if (responseMessage.StatusCode == System.Net.HttpStatusCode.NoContent)
+                return true;
+
+            return false;
         }
     }
 }
