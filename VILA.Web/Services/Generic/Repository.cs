514bbe79174ba96b8921fda58e20efba1812f855
Amir hosein <121675889+Amirvenzee@users.noreply.github.com>
2025-08-24 17:59:45 +0330

    Create Generic Repository

---
+++ VILA.Web/Services/Generic/Repository.cs
@@ -0,0 +1,36 @@
+﻿namespace VILA.Web.Services.Generic
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly IHttpClientFactory _client;
+        public Repository(IHttpClientFactory client)
+        {
+            _client = client;
+        }
+
+        public Task<bool> Create(string url, string token, T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(string url, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAll(string url, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetById(string url, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(string url, string token, T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
