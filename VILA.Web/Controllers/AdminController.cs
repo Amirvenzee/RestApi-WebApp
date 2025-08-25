22f2ab3e6bbe9e935e3d11bb85cc64cf3725038d
Amir hosein <121675889+Amirvenzee@users.noreply.github.com>
2025-08-25 22:02:05 +0330

    Create AllVilas

--- VILA.Web/Controllers/AdminController.cs
+++ VILA.Web/Controllers/AdminController.cs
@@ -1,14 +1,35 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+using VILA.Web.Services.Customer;
+using VILA.Web.Services.Vila;
+using VILA.Web.Utility;
 
 namespace VILA.Web.Controllers
 {
     [Authorize(Roles ="admin")]
     public class AdminController : Controller
     {
-        public IActionResult AllVilas()
+        private readonly IAuthService _auth;
+        private readonly IVilaRepository _vila;
+        private readonly ApiUrls _apiurl;
+
+
+        public AdminController(IAuthService auth, IVilaRepository vila,IOptions<ApiUrls> url)
         {
-            return View();
+            _auth = auth;
+            _vila = vila;
+            _apiurl = url.Value;
+        }
+
+
+        public async Task<IActionResult> AllVilas()
+        {
+            var seccret = _auth.GetJwtToken();
+            var url = $"{_apiurl.BaseAddress}{_apiurl.VilaV1Address}";
+            var model = await _vila.GetAll(url, seccret);
+            return View(model);
         }
     }
 }
