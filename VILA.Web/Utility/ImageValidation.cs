8db16878e8ca8a5ff21cd903df9fbf1e592229cb
Amir hosein <121675889+Amirvenzee@users.noreply.github.com>
2025-09-06 14:45:54 +0330

    Create ImageValidation

---
+++ VILA.Web/Utility/ImageValidation.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Drawing;
+
+namespace VILA.Web.Utility
+{
+    public static class ImageValidation
+    {
+        public static bool IsImage(this IFormFile file)
+        {
+            try
+            {
+                var image = Image.FromStream(file.OpenReadStream());
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
