d52bd95e37c3e6540abb0b9c41f777ab166d2e3a
Amir hosein <121675889+Amirvenzee@users.noreply.github.com>
2025-09-09 14:07:19 +0330

    Create Detail Repository

---
+++ VILA.Web/Services/Detail/DetailRepository.cs
@@ -0,0 +1,17 @@
+﻿using VILA.Web.Models.Detail;
+using VILA.Web.Services.Generic;
+
+namespace VILA.Web.Services.Detail
+{
+    public class DetailRepository:Repository<DetailModel>,IDetailRepository
+    {
+        private readonly IHttpClientFactory _Client;
+
+        public DetailRepository(IHttpClientFactory client):base(client)
+        {
+            _Client = client;
+        }
+
+        
+    }
+}
---
+++ VILA.Web/Services/Detail/IDetailRepository.cs
@@ -0,0 +1,9 @@
+﻿using VILA.Web.Models.Detail;
+using VILA.Web.Services.Generic;
+
+namespace VILA.Web.Services.Detail
+{
+    public interface IDetailRepository:IRepository<DetailModel>
+    {
+    }
+}
